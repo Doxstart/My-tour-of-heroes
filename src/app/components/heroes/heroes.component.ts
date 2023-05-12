d2bf0dddbe629070a081c5ff0d8275ab4bb8f306
Doxstart <127298109+Doxstart@users.noreply.github.com>
2023-05-12 13:09:48 +0200

    first commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,45 @@
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { Hero } from 'src/app/model/hero';
+import { HEROES } from 'src/app/model/mock-heroes';
+import { HeroService } from 'src/app/services/hero/hero.service';
+import { MessageService } from 'src/app/services/message/message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit{
+
+  //hero: Hero = {id: 1, name: 'windstorm'}
+  heroes?: Hero[];
+  selectedHero?: Hero;
+  messageService: any;
+
+  constructor(private heroS:HeroService, private messageS: MessageService){}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+
+
+
+  getHeroes(){
+    this.heroS.getHeroes().subscribe({
+      next: newHeroes => this.heroes = newHeroes,
+      error: err => console.log(err)
+    })
+
+  }
+
+
+  onSelect(hero:Hero){
+    console.log(hero);
+    this.selectedHero=hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+}
+
+
